8fd9b83af586dac4111196e7390d8caf5c92b8a5
Imtiaz Akib <imtiazakib007@gmail.com>
2023-04-11 14:53:38 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,59 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
+import state from "../store";
+import { CustomButton } from "../components";
+
+const Home = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                let's <br className="xl:block hidden" /> Do It
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Shirt design is the process of creating a visual representation
+                of a shirt's appearance, style, and features. It involves
+                various elements such as
+                <strong>color, pattern, fabric, and graphics</strong> Shirt
+                designs can vary widely depending on the intended audience,
+                purpose, and occasion.
+              </p>
+
+              <CustomButton
+                type="filled"
+                title="Customize it"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
